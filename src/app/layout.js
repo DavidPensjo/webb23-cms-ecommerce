2dfaedd3f301209f520ebda7a1227803b2802b63
DavidPensjo <david.pensjo@gmail.com>
2024-09-09 11:13:24 +0200

    Typography and color implement

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import StoryblokProvider from "@/providers/StoryblokProvider";
 import { StoryblokCMS } from "@/utils/cms";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
+import { Head } from "next/document";
 
 storyblokInit({
   accessToken: StoryblokCMS.TOKEN,
@@ -14,6 +15,12 @@ export default async function RootLayout({ children }) {
   return (
     <StoryblokProvider>
       <html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Layout config={currentConfig}>{children}</Layout>
         </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["Public Sans", "sans-serif"],
+      },
+      colors: {
+        fill_primary: "#ff6347",
+        fill_secondary: "#32cd32",
+        fill_bg_primary: "#1e90ff",
+        text_primary: "#333333",
+        text_secondary: "#ffffff",
+      },
+      fontSize: {
+        h1: ["72px", "80px"],
+        h2: ["28px", "36px"],
+        h3: ["24px", "32px"],
+        h4: ["20px", "28px"],
+        h5: ["16px", "24px"],
+        h6: ["14px", "20px"],
+      },
     },
   },
   plugins: [],
