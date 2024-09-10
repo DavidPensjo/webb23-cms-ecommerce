4349f84326de7ff75c94b4e8e317fda5ca2ffee3
mazmann <sadi.lundberg@hotmail.com>
2024-09-10 09:57:21 +0200

    added categories to shoplistpage

---
+++ src/components/nestable/CategoryNav.jsx
@@ -0,0 +1,14 @@
+import ButtonRectangle from './ButtonRectangle';
+
+export default function CategoryNav({ blok }) {
+    console.log(blok);
+    return (
+        <section className="flex w-full sm:justify-start justify-center max-w-[1120px] mx-auto">
+        <nav className="flex flex-wrap gap-5 py-8 sm:py-14 max-w-screen-xl sm:justify-start justify-center">
+            {blok.rectangles.map((rectangle, index) => (
+                <ButtonRectangle key={index} blok={rectangle} />
+            ))}
+        </nav>
+        </section>
+    );
+}
