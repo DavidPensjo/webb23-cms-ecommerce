235c5f451db9b0db36ec6ec3287bbffa4839ce29
mazmann <sadi.lundberg@hotmail.com>
2024-09-10 10:52:21 +0200

    improved responsibility on shoplistcomponents

--- src/components/nestable/ShopListHero.jsx
+++ src/components/nestable/ShopListHero.jsx
@@ -1,8 +1,8 @@
 export default function Hero({ blok }) {
   return (
   
-    <section className="flex w-full max-w-default mt-5">
-      <div className="w-full md:w-1/2 pr-0 flex flex-col justify-center md:text-left mb-8 md:mb-0">
+    <section className="flex w-full lg:justify-start justify-center max-w-default mt-5">
+      <div className="w-full md:w-1/2 pr-0 flex flex-col lg:justify-start justify-center mb-8 md:mb-0">
         <h1 className="text-2xl md:text-4xl font-bold mb-4">
           {blok?.title}
         </h1>
