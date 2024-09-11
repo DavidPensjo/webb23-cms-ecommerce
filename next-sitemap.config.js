d9f0d17de55234b44c439ee076ddcc63a20587fb
mazmann <sadi.lundberg@hotmail.com>
2024-09-11 19:30:26 +0200

    sitemap fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: 'https://webb23-cms-ecommerce.vercel.app',
   generateRobotsTxt: true,
-  sitemapSize: 5000, 
+  sitemapSize: 50000, 
   changefreq: 'daily', 
   priority: 0.7, 
   generateIndexSitemap: true, 
