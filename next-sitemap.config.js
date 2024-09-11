0bdab7371cad2d960f21d78207f9f5024a2a3a76
mazmann <sadi.lundberg@hotmail.com>
2024-09-11 19:37:03 +0200

    final sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,9 @@ module.exports = {
   siteUrl: 'https://webb23-cms-ecommerce.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 50000, 
-  changefreq: 'daily', 
-  priority: 0.7, 
-  generateIndexSitemap: true, 
+  transform: async (config, path) => {
+    return {
+      loc: path, 
+    };
+  },
 };
