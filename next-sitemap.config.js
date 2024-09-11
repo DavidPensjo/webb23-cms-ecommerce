d32103a140a45e941d4d5d93f95d1a4aaee63233
mazmann <sadi.lundberg@hotmail.com>
2024-09-11 19:25:30 +0200

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://webb23-cms-ecommerce.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 5000, 
+  changefreq: 'daily', 
+  priority: 0.7, 
+  generateIndexSitemap: true, 
+  transform: async (config, path) => {
+    return {
+      loc: path, 
+    };
+  },
+  additionalPaths: async (config) => [
+   
+    {
+      loc: '/products/123', 
+      changefreq: 'daily',
+      priority: 0.7,
+    },
+  ],
+};
