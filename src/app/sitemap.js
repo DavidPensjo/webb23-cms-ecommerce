a324e0abf86ddade3f699f8d0f00cf51731c1aae
DavidPensjo <david.pensjo@gmail.com>
2024-09-13 12:10:09 +0200

    Fixing sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,19 @@
-export default function sitemap() {
-    return [
-        {
-            url: "/",
-        },
-      
-    ]
+import { StoryblokCMS } from "@/utils/cms";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: StoryblokCMS.TOKEN,
+  use: [apiPlugin],
+});
+
+export default async function sitemap() {
+  const pages = await StoryblokCMS.getStaticPaths();
+  console.log(pages);
+
+  const sitemap = pages.map((page) => {
+    return {
+      url: page,
+    };
+  });
+  return sitemap;
 }
